8dc50ff6d91e508a1e4a52d7a01f3c30c51a3155
s1270166 <69298409+s1270166@users.noreply.github.com>
2020-08-06 18:09:17 +0900

    Add files via upload
    HoT: Initial revision

---
+++ HeadTail.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (){
+  int i;
+  int n;
+  int a=0,b=0;
+  
+  printf("Tossing a coin...\n");
+
+  srand(time(NULL));
+  int c[3]={rand(),rand(),rand()};
+
+  for(i=0;i<3;i++){
+    if(c[i]%2==0) { printf("Round %d:Heads\n",i+1);
+      a++;
+    } else if(c[i]%2==1) { 
+      printf("Round %d:Tails\n",i+1);
+      b++;
+    }
+  }
+
+  printf("Heads: %d,Tails:%d\n",a,b);
+
+  return 0;
+}
